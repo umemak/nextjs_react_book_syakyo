67a63af184753106804eb0df4734fc94d0bfdab3
umemak <umematsu@gmail.com>
2022-09-04 11:12:02 +0900

    add fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from 'components/layout'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
