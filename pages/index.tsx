e80e950cc07e3a0e0ffaf04528011d27471341af
umemak <umematsu@gmail.com>
2022-09-04 12:19:00 +0900

    meta (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
+import Meta from 'components/meta'
 import Container from "components/container"
 import Hero from 'components/hero'
 
 const Home: NextPage = () => {
   return (
     <Container>
+      <Meta />
       <Hero
         title="CUBE"
         subtitle="アウトプットしていくサイト"
